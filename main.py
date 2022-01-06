e9ab9eded7b00ae4ca90f5dd1fb6339193349afe
StefanAdamovic <stef.adamovic@gmail.com>
2022-01-06 11:05:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ snake = Snake()
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
-screen.onkey(snake.left,"Left")
-screen.onkey(snake.right,"Right")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 
 game_is_on = True
